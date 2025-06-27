dd97f682bd993e8085025b89de7973c9ff14d64c
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-06-27 20:14:50 +0100

    upd vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3003,
-    allowedHosts: ['pdf.nakul.one'] as any,
+    allowedHosts: ['pdf.nakul.one', 'pdfbuddy.site', 'www.pdfbuddy.site'] as any,
   } as any,
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
