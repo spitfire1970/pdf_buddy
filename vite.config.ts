0e33936ef61f110f8dceec40f08602b1d1bd6508
Spitfire1970 <zcabgoy@ucl.ac.uk>
2025-07-02 00:14:05 +0100

    change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "dist",
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
